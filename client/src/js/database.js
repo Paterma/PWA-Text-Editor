4b95b693182bcf11083c2c84058ae2f585166cfe
Maria Paterno <mariapaterno7@yahoo.com>
2022-04-07 19:19:45 -0600

    added to database and installl

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (value, id) => {
+  console.log('PUT from the database');
+  const textDb = await openDB('text', 1);
+  const tx = textDb.transaction('text', 'readwrite');
+  const store = tx.objectStore('text');
+  const request = store.put({ id: id, value: value });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+console.log('GET from the database');
+const textDb = await openDB('text', 1);
+const tx = textDb.transaction('text', 'readwrite');
+const store = tx.objectStore('text');
+const request = store.get(id); //getAll()
+const result = await request;
+console.log('result.value', result);
+return result;
+}
 initdb();
+
+
+//console.error('getDb not implemented');
+//console.error('putDb not implemented');
